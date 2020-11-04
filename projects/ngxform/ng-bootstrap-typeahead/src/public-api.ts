baac6121fcae2d3206e6231a55fc16770695f8f5
hieupv <hieupv@codersvn.com>
2020-11-04 15:25:17 +0700

    gitsubmodule

--- projects/ngxform/ng-bootstrap-typeahead/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-export * from './lib/controls/basic/ng-bootstrap-typeahead.service';
-export * from './lib/controls/basic/ng-bootstrap-typeahead.component';
-export * from './lib/ng-bootstrap-typeahead.module';
-export * from './lib/controls/basic/ngx-bootstrap-typeahead-control';
-export * from './lib/typeahead/typeahead.module';
-export * from './lib/typeahead/typeahead-window';
-export * from './lib/controls/extend/ngx-bootstrap-typeahead-extend-control';
